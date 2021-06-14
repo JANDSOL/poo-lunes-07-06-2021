88676f5aeb7b6b9e1f0f6796963aef50c58ec2a0
Juan Andrade <jpas.juan23@gmail.com>
2021-06-13 21:46:44 -0500

    Exercise 2 complete

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
-number = 45
-if type(number) == int:
-    print("~ Resultado:", number * 2)
-else:
-    print("~ Este dato NO es numérico.")
+# number = 45
+# if type(number) == int:
+#     print("~ Resultado:", number * 2)
+# else:
+#     print("~ Este dato NO es numérico.")
 
-def message(men):
-    print(men)
+# def message(men):
+#     print(men)
 
 
-message(" ! Primer Programa.")
-message(" ! Segundo Programa.")
+# message(" ! Primer Programa.")
+# message(" ! Segundo Programa.")
+
+
+class Sintax:
+    def __init__(self, data = "~ Iniciando..."):
+        self.word = data
+
+    def useVariables(self):
+        age, _weight = 19, 50
+        name = "Juan Andrade"
+        type_sex = "Masculino"
+        single = True
+        print(" ! Nombre: {}".format(name))
+        print(" ! Edad: {}".format(age))
+        print(" ! Tipo de Sexo: {}".format(type_sex))
+        print(" ! Soltero: {}".format(single))
+        print(" ! Peso: {}kg".format(_weight))          
+
+
+exercise2 = Sintax()
+print(exercise2.word)
+exercise2.useVariables()
