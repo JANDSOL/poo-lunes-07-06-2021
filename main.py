f8f32283a524269b2df0565b926320937022a9fd
Juan Andrade <jpas.juan23@gmail.com>
2021-06-13 21:37:56 -0500

    Exercise 1 complete.

---
+++ main.py
@@ -0,0 +1,12 @@
+number = 45
+if type(number) == int:
+    print("~ Resultado:", number * 2)
+else:
+    print("~ Este dato NO es numérico.")
+
+def message(men):
+    print(men)
+
+
+message(" ! Primer Programa.")
+message(" ! Segundo Programa.")
