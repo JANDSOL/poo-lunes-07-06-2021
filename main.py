2a0b7ff7ad08b492e248679bff1c93bbf9d42527
Juan Andrade <jpas.juan23@gmail.com>
2021-06-13 21:56:32 -0500

    Exercise 3 complete

--- main.py
+++ main.py
@@ -25,9 +25,54 @@ class Sintax:
         print(" ! Edad: {}".format(age))
         print(" ! Tipo de Sexo: {}".format(type_sex))
         print(" ! Soltero: {}".format(single))
-        print(" ! Peso: {}kg".format(_weight))          
+        print(" ! Peso: {}kg".format(_weight))
 
 
-exercise2 = Sintax()
-print(exercise2.word)
-exercise2.useVariables()
+# exercise2 = Sintax()
+# print(exercise2.word)
+# exercise2.useVariables()
+
+
+class Sintax:
+    def __init__(self, data = "~ Iniciando..."):
+        self.word = data
+
+    def useVariables(self):
+        # age, _weight = 19, 50
+        # name = "Juan Andrade"
+        # type_sex = "Masculino"
+        # single = True
+        # print(" ! Nombre: {}".format(name))
+        # print(" ! Edad: {}".format(age))
+        # print(" ! Tipo de Sexo: {}".format(type_sex))
+        # print(" ! Soltero: {}".format(single))
+        # print(" ! Peso: {}kg".format(_weight))
+        
+        # () -> Data type Tuple.
+        user = ()
+        user = ("jpas23", 2001, "jpas.juan23@gmail.com", True)
+
+        # [] -> Data type List.
+        subjects = []
+        subjects = ["Programacion Web", "PHP", "Cobol"]
+        subjects[1] = "Python"
+        subjects.append("Go")
+
+        # {} -> Data type Dictionary.
+        teacher = {}
+        teacher = {"Nombre": "Luis", "Edad": 20, "Fac.": "FACI"}
+        teacher["Carrera"] = "CS"
+
+        # Print with .format().
+        # print("""Me llamo {}, y tengo {}
+        #          años""".format(nombre, edad))
+
+        # Structures Slices.
+        print(user, subjects, teacher)
+        print(user, user[0], user[0:2], user[-1])
+        print(subjects, subjects[2:], subjects[:1], subjects[::], subjects[-2:])
+        print(teacher, teacher["Nombre"])
+
+
+exercise3 = Sintax()
+exercise3.useVariables()
